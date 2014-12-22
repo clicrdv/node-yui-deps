02c16f2795e39127cd12027bbb9324da5fbc7285
Arnaud Didry <arnaud@didry.info>
2014-12-22 03:02:44 +0100

    Update test for Zombie.js

--- tests/test.js
+++ tests/test.js
@@ -1,25 +1,21 @@
 
-var deps = require('../lib/yui-deps.js'),
-    YUI  = require('yui').YUI;
+var deps = require('../index.js');
 
-function setup (Y, config) {
-   console.log('setup', config);
-   if(!config.groups.inputex){
-      config.groups.inputex = {};
-   }
-   config.groups.inputex.base = '/javascripts/lib/inputex-3.2.0/build/';
-   config.base = '/javascripts/lib/yui-3.9.1/';
-}
+var setup =
+   'if(!YUI_config.groups.inputex){' +
+   '   YUI_config.groups.inputex = {};' +
+   '}' +
+   'YUI_config.groups.inputex.base = "/javascripts/lib/inputex-3.2.0/build/";' +
+   'YUI_config.base = "/javascripts/lib/yui-3.9.1/";';
 
 deps({
-   YUI: YUI,
    langs: ['fr', 'de', 'es', 'en', 'nl'],
    loaders: [
     '../node_modules/yui/yui/yui.js',
     'customLoader.js'
    ],
    require: ['custom-init'],
-   use:["history-hash-ie"],
+   use:['history-hash-ie'],
    splitLangs: true,
    packageName: 'package_test',
    setup: setup
